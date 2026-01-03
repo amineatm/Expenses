c5eb5c2b730045f9246203ee2aa93b68c8651b79
Amine <84468278+amineatm@users.noreply.github.com>
2026-01-03 01:14:06 -0600

    list page

--- Expenses.Client/src/app/components/transaction-list/transaction-list.ts
+++ Expenses.Client/src/app/components/transaction-list/transaction-list.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { Transaction } from '../../models/transaction';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-transaction-list',
-  imports: [],
   templateUrl: './transaction-list.html',
   styleUrl: './transaction-list.css',
+  imports:[CommonModule]
 })
 export class TransactionList {
-
+  transactions: Transaction[] = [
+    {
+      id: 1,
+      type:'Expenses' ,
+      category: 'Food',
+      amount: 120,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ];
 }
