143d13db88007fd3f0d3984ef362f49b62b08f61
Amine <84468278+amineatm@users.noreply.github.com>
2026-01-02 19:27:27 -0600

    Create Empty Components

--- Expenses.Client/src/app/app.ts
+++ Expenses.Client/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
+import { TransactionForm } from "./transaction-form/transaction-form";
+import { TransactionList } from "./transaction-list/transaction-list";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, TransactionForm, TransactionList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
