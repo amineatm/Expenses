be2f77ba42949608a3821745ec1de5c5eeedd303
Amine <84468278+amineatm@users.noreply.github.com>
2026-01-03 14:22:55 -0600

    creating form

--- Expenses.Client/src/app/components/transaction-form/transaction-form.ts
+++ Expenses.Client/src/app/components/transaction-form/transaction-form.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-transaction-form',
-  imports: [],
+  imports: [ ReactiveFormsModule],
   templateUrl: './transaction-form.html',
   styleUrl: './transaction-form.css',
 })
-export class TransactionForm {
+export class TransactionForm implements OnInit {
+  transactionForm: FormGroup;
 
+  incomeCategories = [
+    'Salary', 'Freelance', 'Investment'
+  ];
+
+  ExpensesCategories = [
+    'Food', 'Transaportation', 'Entertainment'
+  ];
+
+  availableCategories: string[] = [];
+
+  constructor(private fb: FormBuilder) {
+    this.transactionForm = this.fb.group({
+      type: ['Expenses', Validators.required],
+      category: ['', Validators.required],
+      amount: ['', [Validators.min(0)]],
+      createdAt: [new Date(), Validators.required],
+    })
+  }
+  ngOnInit(): void {
+    const type = this.transactionForm.get('type')?.value;
+    this.availableCategories = type === 'Expenses' ? this.ExpensesCategories : this.incomeCategories;
+    this.transactionForm.patchValue({ category: '' })
+  }
+  onTypeChange() {
+
+  }
+
+  cancel() {
+
+  }
+  OnSubmit() {
+
+  }
 }
