edefc22aab60c475e5d4846e5f335bbcc867e615
Amine <84468278+amineatm@users.noreply.github.com>
2026-01-07 22:33:56 -0600

    loginForm

--- Expenses.Client/src/app/components/header/header.ts
+++ Expenses.Client/src/app/components/header/header.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { Auth } from '../../services/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.html',
   styleUrl: './header.css',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
 })
 export class Header {
+  currentUser$;
 
+  constructor(private authService: Auth) {
+    this.currentUser$ = this.authService.currentUser$;
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
--- Expenses.Client/src/app/services/auth.ts
+++ Expenses.Client/src/app/services/auth.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user';
-import { Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { AuthResponse } from '../models/authresponse';
 import { Router } from '@angular/router';
 
@@ -12,29 +12,40 @@ import { Router } from '@angular/router';
 export class Auth {
   private apiUrl = environment.apiHost + '/Auth';
 
+  private currentUserSubject = new BehaviorSubject<string | null>(null);
+  currentUser$ = this.currentUserSubject.asObservable();
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.currentUserSubject.next('user');
+    }
+  }
 
   login(credentials: User): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(this.apiUrl + "/Login", credentials)
       .pipe(tap((response) => {
         localStorage.setItem('token', response.token)
+        this.currentUserSubject.next('user');
       }));
   }
 
   register(credentials: User): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(this.apiUrl + "/Register", credentials)
       .pipe(tap((response) => {
-        localStorage.setItem('token', response.token)
+        localStorage.setItem('token', response.token);
+        this.currentUserSubject.next('user');
       }))
   }
 
   logout(): void {
     localStorage.removeItem("token");
+    this.currentUserSubject.next(null);
     this.router.navigate(['/login']);
   }
 
   IsAuthenticated(): boolean {
     return !!localStorage.getItem('token');
+
   }
 }
